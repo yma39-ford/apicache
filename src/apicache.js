1f4307b035f5dd8991561aae363184dc3f0792c7
Marko Ma <yma39@ford.com>
2021-06-15 10:48:03 -0700

    temperary fix the issue of browser ignoring cleared cache group

--- src/apicache.js
+++ src/apicache.js
@@ -249,15 +249,15 @@ function ApiCache() {
 
     var headers = getSafeHeaders(response)
 
-    Object.assign(headers, filterBlacklistedHeaders(cacheObject.headers || {}), {
-      // set properly-decremented max-age header.  This ensures that max-age is in sync with the cache expiration.
-      'cache-control':
-        'max-age=' +
-        Math.max(
-          0,
-          (duration / 1000 - (new Date().getTime() / 1000 - cacheObject.timestamp)).toFixed(0)
-        ),
-    })
+    // Object.assign(headers, filterBlacklistedHeaders(cacheObject.headers || {}), {
+    //   // set properly-decremented max-age header.  This ensures that max-age is in sync with the cache expiration.
+    //   'cache-control':
+    //     'max-age=' +
+    //     Math.max(
+    //       0,
+    //       (duration / 1000 - (new Date().getTime() / 1000 - cacheObject.timestamp)).toFixed(0)
+    //     ),
+    // })
 
     // only embed apicache headers when not in production environment
     if (process.env.NODE_ENV !== 'production') {
